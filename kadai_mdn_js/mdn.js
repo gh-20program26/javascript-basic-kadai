b2c7d064955369ede3e1c79632537883ac27112d
gh-20program26 <uu4883765@gmail.com>
2026-05-11 13:14:14 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,4 +0,0 @@
-const today = new Date();
-const [month, day, year] = [today.getMonth()+1, today.getDate(), today.getFullYear(),];
-const date = year + "年" + month + "月" + day + "日";
-console.log(date);
